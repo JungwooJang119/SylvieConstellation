5791e04909cfd61d3e972d2792706e9d02983d74
JungwooJang0911 <jungwoojang2001@gmail.com>
2024-01-18 02:50:38 -0500

    Space bg working wahoo

--- Assets/InputSystem/PlayerInput.cs
+++ Assets/InputSystem/PlayerInput.cs
@@ -53,6 +53,15 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Boost"",
+                    ""type"": ""Button"",
+                    ""id"": ""50c0d98b-5cd1-411e-96b8-ee8ef84bc84d"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -273,6 +282,17 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""groups"": ""XR"",
                     ""action"": ""Fire"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""4eab00d8-9852-49a4-8711-eb8a9b0915bd"",
+                    ""path"": ""<Keyboard>/shift"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard&Mouse"",
+                    ""action"": ""Boost"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -863,6 +883,7 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
         m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
         m_Player_Fire = m_Player.FindAction("Fire", throwIfNotFound: true);
+        m_Player_Boost = m_Player.FindAction("Boost", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -939,6 +960,7 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_Move;
     private readonly InputAction m_Player_Look;
     private readonly InputAction m_Player_Fire;
+    private readonly InputAction m_Player_Boost;
     public struct PlayerActions
     {
         private @PlayerInput m_Wrapper;
@@ -946,6 +968,7 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
         public InputAction @Move => m_Wrapper.m_Player_Move;
         public InputAction @Look => m_Wrapper.m_Player_Look;
         public InputAction @Fire => m_Wrapper.m_Player_Fire;
+        public InputAction @Boost => m_Wrapper.m_Player_Boost;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -964,6 +987,9 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
             @Fire.started += instance.OnFire;
             @Fire.performed += instance.OnFire;
             @Fire.canceled += instance.OnFire;
+            @Boost.started += instance.OnBoost;
+            @Boost.performed += instance.OnBoost;
+            @Boost.canceled += instance.OnBoost;
         }
 
         private void UnregisterCallbacks(IPlayerActions instance)
@@ -977,6 +1003,9 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
             @Fire.started -= instance.OnFire;
             @Fire.performed -= instance.OnFire;
             @Fire.canceled -= instance.OnFire;
+            @Boost.started -= instance.OnBoost;
+            @Boost.performed -= instance.OnBoost;
+            @Boost.canceled -= instance.OnBoost;
         }
 
         public void RemoveCallbacks(IPlayerActions instance)
@@ -1162,6 +1191,7 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
         void OnMove(InputAction.CallbackContext context);
         void OnLook(InputAction.CallbackContext context);
         void OnFire(InputAction.CallbackContext context);
+        void OnBoost(InputAction.CallbackContext context);
     }
     public interface IUIActions
     {
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -21,6 +21,19 @@ public class PlayerController : Singleton<PlayerController>
     public float decceleration;
     public float velPower;
 
+    [SerializeField] private Material bgClose;
+    [SerializeField] private Material bgFar;
+    [SerializeField] private float parallax;
+
+    private Vector2 bgCloseOffset;
+    private Vector2 bgFarOffset;
+
+    [SerializeField] private float boostTime;
+    private float holdSpeed;
+    public float currTime;
+    private bool isBoosted;
+    public ParticleSystem starBits;
+
     private void Awake() {
         InitializeSingleton();
         input = new PlayerInput();
@@ -33,11 +46,19 @@ public class PlayerController : Singleton<PlayerController>
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         canMove = true;
+        holdSpeed = speed;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (isBoosted) {
+            currTime += Time.deltaTime;
+        }
+        if (currTime >= boostTime) {
+            speed = holdSpeed;
+            isBoosted = false;
+        }
         if (canMove) {
             inputVector = input.Player.Move.ReadValue<Vector2>();
             anim.SetFloat("X", inputVector.x);
@@ -52,9 +73,18 @@ public class PlayerController : Singleton<PlayerController>
             float movementY = Mathf.Pow(Mathf.Abs(speedDiffY) * accelRateY, velPower) * Mathf.Sign(speedDiffY);
             rb.AddForce(movementX * Vector2.right);
             rb.AddForce(movementY * Vector2.up);
+            bgCloseOffset.x = transform.position.x / transform.localScale.x / parallax;
+            bgCloseOffset.y = transform.position.y / transform.localScale.y / parallax;
+            bgClose.SetVector("_Offset", new Vector2(bgCloseOffset.x, bgCloseOffset.y));
+            bgFarOffset.x = transform.position.x / transform.localScale.x / (parallax * 10);
+            bgFarOffset.y = transform.position.y / transform.localScale.y / (parallax * 10);
+            bgFar.SetVector("_Offset", new Vector2(bgFarOffset.x, bgFarOffset.y));
             if (inputVector.magnitude > 0) {
                 Vector2 normMovement = inputVector.normalized;
                 anim.SetBool("isMoving", true);
+                if (isBoosted) {
+                    starBits.Play();
+                }
             } else {
                 anim.SetBool("isMoving", false);
             }
@@ -62,6 +92,12 @@ public class PlayerController : Singleton<PlayerController>
     }
 
     private void OnMove(InputValue movementValue) {
+    }
+
+    private void OnBoost() {
+        isBoosted = true;
+        currTime = 0;
+        speed = holdSpeed * 3;
     }
 
     private void OnEnable() {
