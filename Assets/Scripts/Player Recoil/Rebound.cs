e45966f267e754de70a5808f4762217c3b67cb14
Arshiya Rahman <arshiyar2005@gmail.com>
2024-01-28 14:28:35 -0500

    Refine Player Recoil
    - added/edited capsule colliders of the obstacle and Sylvie
    - created Rebound script that is attached to the obstacle: this script uses RigidBody2D.AddForce() to add a force proportional to Sylvie's velocity in the opposite direction

--- Assets/Scripts/Player Recoil/Rebound.cs
+++ Assets/Scripts/Player Recoil/Rebound.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class Rebound : MonoBehaviour
 {
     [SerializeField] private float forceMag = 10;
-   
     public void OnTriggerEnter2D(Collider2D other) {
         Debug.Log("collided!");
         if (other.gameObject.tag == "Player") {
             Rigidbody2D playerBody = other.gameObject.GetComponent<Rigidbody2D>();
-            Vector2 forceApplied = new Vector2(/*-1 * playerBody.velocity.normalized.x **/ forceMag, /*-1 * playerBody.velocity.normalized.y **/ forceMag);
+            Vector2 forceApplied = new Vector2(-0.5f * playerBody.velocity.x * forceMag, -0.5f * playerBody.velocity.y * forceMag);
             playerBody.AddForce(forceApplied, ForceMode2D.Impulse);
         }
     }
