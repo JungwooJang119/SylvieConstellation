21071691c61ca05d86484099b147f2b6bf0b3504
tateJohnson <46613333+tateJohnson@users.noreply.github.com>
2024-02-12 19:52:24 -0500

    Refactoring
    turned button scaling into a co-routine

--- Assets/Scripts/UI/ButtonExpand.cs
+++ Assets/Scripts/UI/ButtonExpand.cs
@@ -5,64 +5,58 @@ using UnityEngine;
 
 public class ButtonExpand : MonoBehaviour
 {
-    //different states the button is in
-    enum ButtonState
-    {
-        REST, GROW, SHRINK
-    }
 
     //speed of button movement
     [SerializeField] float buttonSpeed = 0.02f;
     //% of exspantion
     [SerializeField] float buttonScaler = 1.5f;
 
-    ButtonState buttState;
     Vector3 movement;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         movement = Vector3.one * buttonSpeed;
-
-        buttState = ButtonState.REST;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //checks to scale button
-        switch (buttState)
-        {
-            case ButtonState.REST:
-            break;
 
-            case ButtonState.GROW:
-            transform.localScale += movement;
-            break;
-
-            case ButtonState.SHRINK:
-            transform.localScale -= movement;
-            break;
-
-            default:
-            break;
-        }
-
-        //stops button from scaling
-        if (buttState > ButtonState.REST && (transform.localScale.y <= 1.0f || transform.localScale.y >= buttonScaler))
-        {
-            buttState = ButtonState.REST;
-        }
     }
 
     //called when pointer enters
     public void ButtonGrow(GameObject button){
-
-        buttState = ButtonState.GROW;
+        StopAllCoroutines();
+        StartCoroutine(Grow(true));;
     }
 
     //called when pointer exits
     public void ButtonShrink(GameObject button){
-        buttState = ButtonState.SHRINK;
+        StopAllCoroutines();
+        StartCoroutine(Grow(false));;
+    }
+
+    public IEnumerator Grow(bool growing) {
+        if (growing)
+        {
+            while (transform.localScale.y < buttonScaler)
+            {
+                transform.localScale += movement;
+                yield return null;
+                Debug.Log("big");
+            }
+
+        } else {
+            while (transform.localScale.y > 1.0f)
+            {
+                transform.localScale -= movement;
+                yield return null;
+                Debug.Log("small");
+            }
+
+        }
     }
 }
