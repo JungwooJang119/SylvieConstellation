24238d9f14a9812ba24ce24cf0fdde6b935223c9
chrysly <chrysly0816@gmail.com>
2024-02-20 11:55:23 -0500

    Black Hole Implementation+Vis

---
+++ Assets/Scripts/Misc/Notes.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour {
+    [TextArea] [Tooltip("Notes")]
+    public string notes = "Add text here";
+}
