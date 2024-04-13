087dc95c4ad3fc8093ad2e2dc8b96a2d8c918a64
Carlijavier5 <carlijavier5@gmail.com>
2024-04-13 01:50:48 -0400

    Cleanup

--- Assets/Scenes/Carlos/Draco/Puzzle Logic/DracoPuzzleManager.cs
+++ Assets/Scenes/Carlos/Draco/Puzzle Logic/DracoPuzzleManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PuzzleManagement;
 
 public class DracoPuzzleManager : MonoBehaviour {
 
+    [SerializeField] private PuzzleID puzzleID;
     [SerializeField] private DracoConstellationBrain puzzleBrain;
     [SerializeField] private float puzzleBeginDelay;
 
@@ -13,6 +15,10 @@ public class DracoPuzzleManager : MonoBehaviour {
 
     private void PuzzleBrain_OnPuzzleEnd(bool success) {
         /// Here goes what happens after the puzzle ends;
+        PuzzleManager pm;
+        if ((pm = PuzzleManager.Instance) != null) pm.CompletePuzzle(puzzleID);
+
+        Debug.Log("You won/lost!");
     }
 
     public void StartPuzzle() => StartCoroutine(_BeginPuzzle(puzzleBeginDelay));
