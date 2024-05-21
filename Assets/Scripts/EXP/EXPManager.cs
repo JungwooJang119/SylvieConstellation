35fc99aa9bf3c2bc920219dc12465601479529ea
Arshiya Rahman <arshiyar2005@gmail.com>
2024-05-21 16:12:32 -0400

    Complete Level-Up w/out Saving

--- Assets/Scripts/EXP/EXPManager.cs
+++ Assets/Scripts/EXP/EXPManager.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-enum SYLVIE_STAGE {
-    BABY, CHILD, TEEN, ADULT
+//enum to track level progression
+public enum SYLVIE_STAGE {
+    BABY , CHILD, TEEN, ADULT
 }
 public class EXPManager : MonoBehaviour
 {
     //total EXP that the player has collected
-    [SerializeField] private static int currentExp = 0;
+    [SerializeField] private int currentExp = 0;
     //EXP that the player still needs to level up
-    [SerializeField] private static int expUntilLevel = 100;
+    [SerializeField] private int expUntilLevel = 100;
     //the current stage that needs to be displayed
-    [SerializeField] private static SYLVIE_STAGE stage = SYLVIE_STAGE.BABY;
+    [SerializeField] private SYLVIE_STAGE stage = SYLVIE_STAGE.BABY;
     //potential sprites
+    [SerializeField] public Sprite babySprite;
     [SerializeField] public Sprite childSprite;
     [SerializeField] public Sprite teenSprite;
     [SerializeField] public Sprite adultSprite;
@@ -21,28 +23,42 @@ public class EXPManager : MonoBehaviour
     [SerializeField] public GameObject player;
     //reference to the puzzle manager object
     [SerializeField] public GameObject puzzleManager;
-    //the current puzzle, is 0 in open world
-    [SerializeField] private PuzzleManagement.PuzzleID  puzzle = 0;
-    //the amount to add per level
+    //the current puzzle -> how to update using PuzzleManager
+    [SerializeField] private PuzzleManagement.PuzzleID  puzzle = PuzzleManagement.PuzzleID.MainWorld;
+    //the amount to add per level -> subject to change
     [SerializeField] private int majorEXP = 25;
     [SerializeField] private int minorEXP = 15;
-    public static void addEXP(int exp) {
+
+
+
+    //adds to total EXP for puzzle completion
+    public void addEXP(int exp) {
         if (exp > 0) {
             currentExp += exp;
-            expUntilLevel -= exp;
+            if (stage != SYLVIE_STAGE.ADULT) {
+                expUntilLevel -= exp;
+            } else {
+                return;
+            }
         }
         if (expUntilLevel <= 0) {
             updateLevel();
-            expUntilLevel = 100;
+            if (stage != SYLVIE_STAGE.ADULT) {
+                expUntilLevel = 100;
+            }
         }
     }
-    public static void spendEXP(int exp) {
+    
+    //removes total EXP for the shop
+    //note that this does not take away from level progress
+    public void spendEXP(int exp) {
         if (exp > 0) {
             currentExp -= exp;
         }
     }
 
-    private static void updateLevel() {
+    //transitions Sylvie to the next level when necessary
+    private void updateLevel() {
         switch(stage) {
             case SYLVIE_STAGE.BABY:
                 stage = SYLVIE_STAGE.CHILD;
@@ -54,42 +70,73 @@ public class EXPManager : MonoBehaviour
                 stage = SYLVIE_STAGE.ADULT;
                 break;
         }
+        updateSprite();
     }
 
+    //update Sylvie's appearance based on their level
+    //note that the code functions but the appearance does not change due to the animator
     private void updateSprite() {
+        Debug.Log("updating appearance");
         switch(stage) {
+            case SYLVIE_STAGE.BABY:
+                Debug.Log("baby appearance");
+                player.gameObject.GetComponent<SpriteRenderer>().sprite = babySprite;
+                break;
             case SYLVIE_STAGE.CHILD:
+                Debug.Log("child appearance");
                 player.gameObject.GetComponent<SpriteRenderer>().sprite = childSprite;
                 break;
             case SYLVIE_STAGE.TEEN:
+                Debug.Log("teen appearance");
                 player.gameObject.GetComponent<SpriteRenderer>().sprite = teenSprite;
                 break;
             case SYLVIE_STAGE.ADULT:
+                Debug.Log("adult appearance");
                 player.gameObject.GetComponent<SpriteRenderer>().sprite = adultSprite;
                 break;
         }
     }
-    //fix
+
+    //getter methods for use with the SaveSystem
+    public SYLVIE_STAGE getStage() {
+        return stage;
+    }
+    public int getCurrentExp() {
+        return currentExp;
+    }
+    public int getExptUntilLevel() {
+        return expUntilLevel;
+    }
+    
+    //Setter methods for retrieving from SaveSystem
+    public void GetSaveFile(SYLVIE_STAGE s, int currentE, int eLeft) {
+        currentExp = currentE;
+        expUntilLevel = eLeft;
+        stage = s;
+    }
+    //determines if something is a main puzzle or not -> fix
     private bool isMainPuzzle(PuzzleManagement.PuzzleID p) {
         return true;
     }
 
-    void start() {
+    //adjusts Sylvie's appearance at the start of the load
+    void Start() {
         updateSprite();
     }
-    void update() {
-        Debug.Log("hello");
+    void Update() {
         //for testing
         if(Input.GetKeyDown(KeyCode.Tab)) {
             addEXP(majorEXP);
-            Debug.Log(currentExp + ", ");
+            Debug.Log(currentExp + ", " + expUntilLevel + ", " + stage);
         }
-        // if (puzzleManager.gameObject.GetComponent<PuzzleManagement.PuzzleManager>().GetPuzzleStatus(puzzle)) {
-        //     if (isMainPuzzle(puzzle)) {
-        //         addEXP(majorEXP);
-        //     } else {
-        //         addEXP(minorEXP);
-        //     }
-        // }
+        
+        //for actual gameplay
+        if (puzzleManager.gameObject.GetComponent<PuzzleManagement.PuzzleManager>().GetPuzzleStatus(puzzle)) {
+            if (isMainPuzzle(puzzle)) {
+                addEXP(majorEXP);
+            } else {
+                addEXP(minorEXP);
+            }
+        }
     }
 }
